bb6b3d166b2843c5359b6a61a5003768a3004e48
Adam Fadhel <adamfdhl@gmail.com>
2020-11-03 12:40:18 +0700

    done questionnaire

--- routes/questionnaire.js
+++ routes/questionnaire.js
@@ -7,5 +7,15 @@ router.route("/").get((req, res) => {
 		.catch((err) => res.status(400).json({ Error: err }));
 });
 
+router.route("/add").post((req, res) => {
+  const question = req.body.question
+  const answer_options = req.body.answer_options
+  const correct_answer_idx = req.body.correct_answer_idx
+  const newQuestionnaire = new Questionnaire({question, answer_options, correct_answer_idx})
+  newQuestionnaire
+    .save()
+    .then(() => res.json({text: "Question added!"}))
+    .catch(err => res.status(400).json({Error: err}))
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -22,7 +22,7 @@ connection.once("open", () => {
 })
 
 app.use("/users", userRoute)
-app.use("/questionnaire", questionnaireRoute)
+app.use("/questionnaires", questionnaireRoute)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
